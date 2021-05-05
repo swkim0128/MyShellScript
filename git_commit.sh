a2b8ecd2f06b5c337ef9f5a891ba9acb947b4979
eunsol <eunsol@eunsolui-MacBookAir.local>
2021-05-05 16:53:44 +0900

    ENG: modify git_commit.sh
    새로운 git commit script 작성.
    어떤 오류로 인해 수정한 스크립트가 제대로 커밋이 되지 않는 상황 발생.
    
    다시 스크립트를 실행시켜 해당 내용을 커밋함. 아직 미확인된 오류는 고치지 않음.

--- git_commit.sh
+++ git_commit.sh
@@ -1,2 +1,36 @@
-git add ${1}
-git commit
+#!/bin/bash
+
+cd $(pwd)
+
+modify_file=()
+new_file=()
+
+git status -s >status_temp.txt
+
+while read line
+do
+	git_status=$(echo ${line} | awk '{print $1}')
+	file=$(echo ${line} | awk '{print $2}')
+
+	if [[ ${git_status} = "??" ]]; then
+		new_file+=(${file})
+	else
+		modify_file+=(${file})
+	fi
+	
+done <status_temp.txt
+
+if [[ ${new_file} != "" ]]; then
+	echo "add new_file!"
+	sleep 3s
+
+	git add ${new_file}
+	git commit
+fi
+
+if [[ ${modify_file} != "" ]]; then
+	echo "add modify_file!"
+	sleep 3s
+	git add ${modify_file}
+	git commit
+fi
